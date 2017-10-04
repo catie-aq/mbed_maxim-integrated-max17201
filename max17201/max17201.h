a7403861afd9e04b272bffd0c75fb4ea19708b4d
Clément Pinet <c.pinet@catie.fr>
2017-10-04 10:01:43 +0000

    Uses 6tron doc format : max17201.h

--- max17201/max17201.h
+++ max17201/max17201.h
@@ -23,8 +23,8 @@ namespace sixtron {
 
 #define R_SENSE            0.020 //Value of the sense resistor
 
-/**
- *  @class MAX17201
+/*!
+ *  \class MAX17201
  *  MAX17201 gauge driver
  */
 class MAX17201
@@ -39,19 +39,19 @@ public:
 
     /* Represents the different alert flags for the MAX17048 */
     enum class StatusFlags : uint16_t { // short ?
-        ALERT_POR             = (1 << 1),  /**< Power On Reset Indicator */
-        ALERT_CL              = (1 << 2),  /**< Minimum Current Alert Threshold Exceeded */
-        BATTERY_PRESENT       = (1 << 3),  /**< Battery presence indicator */
-        ALERT_CH              = (1 << 6),  /**< Maximum Current Alert Threshold Exceeded */
-        ALERT_dSOCI           = (1 << 7),  /**< 1% SOC change alert */
-        ALERT_VL              = (1 << 8),  /**< Minimum Voltage Alert Threshold Exceeded */
-        ALERT_TL              = (1 << 9),  /**< Minimum Temperature Alert Threshold Exceeded */
-        ALERT_SL              = (1 << 10), /**< Minimum SOC Alert Threshold Exceeded */
-        ALERT_BI              = (1 << 11), /**< Battery Insertion */
-        ALERT_VH              = (1 << 12), /**< Maximum Voltage Alert Threshold Exceeded */
-        ALERT_TH              = (1 << 13), /**< Maximum Temperature Alert Threshold Exceeded */
-        ALERT_SH              = (1 << 14), /**< Maximum SOC Alert Threshold Exceeded */
-        ALERT_BR              = (1 << 15)  /**< Battery Removal */
+        ALERT_POR             = (1 << 1),  /*!< Power On Reset Indicator */
+        ALERT_CL              = (1 << 2),  /*!< Minimum Current Alert Threshold Exceeded */
+        BATTERY_PRESENT       = (1 << 3),  /*!< Battery presence indicator */
+        ALERT_CH              = (1 << 6),  /*!< Maximum Current Alert Threshold Exceeded */
+        ALERT_dSOCI           = (1 << 7),  /*!< 1% SOC change alert */
+        ALERT_VL              = (1 << 8),  /*!< Minimum Voltage Alert Threshold Exceeded */
+        ALERT_TL              = (1 << 9),  /*!< Minimum Temperature Alert Threshold Exceeded */
+        ALERT_SL              = (1 << 10), /*!< Minimum SOC Alert Threshold Exceeded */
+        ALERT_BI              = (1 << 11), /*!< Battery Insertion */
+        ALERT_VH              = (1 << 12), /*!< Maximum Voltage Alert Threshold Exceeded */
+        ALERT_TH              = (1 << 13), /*!< Maximum Temperature Alert Threshold Exceeded */
+        ALERT_SH              = (1 << 14), /*!< Maximum SOC Alert Threshold Exceeded */
+        ALERT_BR              = (1 << 15)  /*!< Battery Removal */
     };
 
     enum class RegisterAddress : char {
@@ -152,224 +152,224 @@ public:
     MAX17201(I2C* i2c, PinName interruptPin);
     MAX17201(I2C* i2c);
 
-    /**
+    /*!
      *  Configure the gauge
      *
-     *  @param number_of_cells : default value is 1
-     *  @param design_capacity : capacity (mAh), default is 800mAh
-     *  @param empty_voltage : voltage (V) bellow which battery is considered empty, default is 3.1V
-     *  @param use_external_thermistor1 : default is false
-     *  @param use_external_thermistor2 : default is false (in case both are false, will use internal thermistor)
-     *  @return true on success, false on failure
+     *  \param number_of_cells : default value is 1
+     *  \param design_capacity : capacity (mAh), default is 800mAh
+     *  \param empty_voltage : voltage (V) bellow which battery is considered empty, default is 3.1V
+     *  \param use_external_thermistor1 : default is false
+     *  \param use_external_thermistor2 : default is false (in case both are false, will use internal thermistor)
+     *  \return true on success, false on failure
      */
     bool configure(uint8_t number_of_cells = 1, uint16_t design_capacity = 800, float empty_voltage = 3.1,
             bool use_external_thermistor1 = false, bool use_external_thermistor2 = false);
 
-    /**
+    /*!
      *  Get battery state of charge (%)
      *
-     *  @return Percentage of battery remaining
+     *  \return Percentage of battery remaining
      */
     float state_of_charge();
 
-    /**
+    /*!
      *  Get delivered current
      *
-     *  @return current (mA)
+     *  \return current (mA)
      */
     double current();
 
-    /**
+    /*!
      *  Get average current for the last 5.625 s
      *
-     *  @return average current (mA)
+     *  \return average current (mA)
      */
     double average_current();
 
-    /**
+    /*!
      *  Get maximum current since device reset
      *
-     *  @return maximum current (mA)
+     *  \return maximum current (mA)
      *
      */
     float max_current();
 
-    /**
+    /*!
      *  Get minimum current since device reset
      *
-     *  @return minimum current (mA)
+     *  \return minimum current (mA)
      */
     float min_current();
 
-    /**
+    /*!
      *  Get cell voltage
-     * If more than one cell are used this is the lowest voltage of all cells
+     *  If more than one cell are used this is the lowest voltage of all cells
      *
-     *  @return cell voltage
+     *  \return cell voltage
      */
     double cell_voltage();
 
-    /**
+    /*!
      *  Get average cell voltage for the last 45 s
      *
-     *  @return average cell voltage
+     *  \return average cell voltage
      */
     double average_cell_voltage();
 
-    /**
+    /*!
      *  Get maximum cell voltage since device reset
      *
-     *  @return maximum cell voltage
+     *  \return maximum cell voltage
      */
     float max_cell_voltage();
 
-    /**
+    /*!
      *  Get minimum cell voltage since device reset
      *
-     *  @return minimum cell voltage
+     *  \return minimum cell voltage
      */
     float min_cell_voltage();
 
-    /**
+    /*!
      *  Remaining time until battery is full
      *
-     *  @return time to full (seconds)
+     *  \return time to full (seconds)
      */
     float time_to_full();
 
-    /**
+    /*!
      *  Get remaining time until battery is empty
      *
-     *  @return time to empty (seconds)
+     *  \return time to empty (seconds)
      */
     float time_to_empty();
 
-    /**
+    /*!
      *  Get the remaining capacity of the battery
      * 
-     * @returns remaining capacicity (mAh)
+     *  \return remaining capacicity (mAh)
      */
     float reported_capacity();
 
-    /**
+    /*!
      *  Get the full capacity of the battery computed by the gauge algorithm
      *  This is not the design capacity of the battery as the one given in the 
-     * @ref configure() function. Cell ages, temperature, cumber of cycles etc..
-     * are taken into acount to compute the full capacity
+     *  \ref configure() function. Cell ages, temperature, cumber of cycles etc..
+     *  are taken into acount to compute the full capacity
      * 
-     * @returns full capacity (mAh)
+     *  \return full capacity (mAh)
      */
     float full_capacity();
 
-    /**
+    /*!
      *  Get temperature of the enabled thermistor
      *  If more than one thermistor are used, it is an average the used thermistor
      * 
-     *  @return temperature (°C)
+     *  \return temperature (°C)
      */
     float temperature();
 
-    /**
+    /*!
      *  Get the average temperature for a configurable period (default is 1.5min)
      *
-     *  @return The average temperature
+     *  \return The average temperature
      */
     float average_temperature();
 
-    /**
+    /*!
      *  Get the maximum temperature since device reset
      *
-     *  @return Maximum temperature
+     *  \return Maximum temperature
      */
     int8_t max_temperature();
 
-    /**
+    /*!
      *  Get the minimum temperature since device reset
      *
-     *  @return minimum temperature
+     *  \return minimum temperature
      */
     int8_t min_temperature();
 
-    /**
+    /*!
      *  Get the cell age in hours
      * 
-     * @returns cell age (hours)
+     *  \return cell age (hours)
      */
     float age();
 
-    /**
+    /*!
      *  Number of cycle completed (1 cycle is equivalent to 1 full charge and 1 full discharge)
      *
-     *  @return number of cycle
+     *  \return number of cycle
      */
     float cycle_count();
 
-    /**
+    /*!
      *  Set alerts on minimum and maximum current
      *
-     *  @param max_current_threshold (mA)
-     *  @param min_current_threshold (mA)
+     *  \param max_current_threshold (mA)
+     *  \param min_current_threshold (mA)
      */
     void set_current_alerts(float max_current_threshold, float min_current_threshold);
 
-    /**
+    /*!
      *  Set alerts on minimum and maximum voltage
      *
-     *  @param max_voltage_threshold (V)
-     *  @param min_voltage_threshold (V)
+     *  \param max_voltage_threshold (V)
+     *  \param min_voltage_threshold (V)
      */
     void set_voltage_alerts(float max_voltage_threshold, float min_voltage_threshold);
 
-    /**
+    /*!
      *  Set alerts on minimum and maximum battery percentage
      *
-     *  @param max_soc_threshold (%)
-     *  @param min_soc_threshold (%)
+     *  \param max_soc_threshold (%)
+     *  \param min_soc_threshold (%)
      */
     void set_state_of_charge_alerts(uint8_t max_soc_threshold, uint8_t min_soc_threshold);
 
-    /**
+    /*!
      *  Enable alerts on voltage, current and state of charge
      */
     void enable_alerts();
 
-    /**
+    /*!
      *  Disable alerts on voltage, current and state of charge
      */
     void disable_alerts();
 
-    /**
+    /*!
      *  Thoose are the gain and offset, specific to thermistor used, that the gauge
      *  algorithm will use to copute temperature
      *  See the gauge datasheet for more informations about the computations
      */
     void configure_thermistor(uint16_t gain, uint16_t offset);
 
-    /**
+    /*!
      *  Reset registers and gauge algorithm without loading the non-volatile memory backup
      */
     void restart_firmware();
 
-    /**
+    /*!
      *  Full reset of the gauge with with registers restoration from the non-volatile memory backup if enable
      */
     void reset();
 
-    /**
+    /*!
      *  Get the number of remaining updates of the Nonvolatile memory
      *
-     *  @returns The number of remaining updates
+     *  \return The number of remaining updates
      */
     uint8_t remaining_writes();
 
-    /**
+    /*!
      *  Alert related functions
      */
     void handle_alert();
 
 private:
 
-    /**
+    /*!
      *  Configure the empty voltage used by ModelGauge m5 algorithm
      *                   _____________________________________________________________________________________
      *  Register format |_D15_|_D14_|_D13_|_D12_|_D11_|_D10_|_D9_|_D8_|_D7_|_D6_|_D5_|_D4_|_D3_|_D2_|_D1_|_D0_|
@@ -377,14 +377,14 @@ private:
      *  VEmpty is 10mv per LSB
      *  VRecovery is 40mv per LSB
      *
-     *  @param VEmpty The empty voltage to use in Volts
+     *  \param VEmpty The empty voltage to use in Volts
      */
     void set_empty_voltage(float empty_voltage);
 
-    /**
+    /*!
      *  Set battery capacity
      *
-     *  @param battery capacity (mAh)
+     *  \param battery capacity (mAh)
      */
     void set_design_capacity(uint16_t design_capacity);
 
